498917eec1061650977d43dc6cd0e08a6060c6ea
Noureldeen Osama Mohamed <138819436+Noureldeen75@users.noreply.github.com>
2026-06-24 17:39:21 +0300

    added home UI enhancments

--- Waveflow/Views/WeatherPageView.swift
+++ Waveflow/Views/WeatherPageView.swift
@@ -14,44 +14,47 @@ struct WeatherPageView: View {
     let onFavTapped: () -> Void
     
     var body: some View {
-        ZStack {
-            Image(backgroundImageName)
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-            
-            ScrollView {
-                VStack {
-                    HStack {
-                        Spacer()
-                        Button(action: onFavTapped) {
-                            Image(systemName: "heart.fill")
-                                .font(.title2)
-                                .foregroundColor(textColor)
+        GeometryReader { geometry in
+            ZStack {
+                Image(backgroundImageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .clipped()
+                
+                ScrollView {
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button(action: onFavTapped) {
+                                Image(systemName: "heart.fill")
+                                    .font(.title2)
+                                    .foregroundColor(textColor)
+                            }
+                            .padding(.trailing, 20)
                         }
-                        .padding(.trailing, 20)
+                        .padding(.top, 50)
+                        
+                        WeatherHeaderView(
+                            weatherData: weatherData,
+                            textColor: textColor
+                        )
+                        .padding(.top, 20)
+                        
+                        ThreeDayForecastView(
+                            forecastDays: weatherData.forecast,
+                            backgroundImageName: backgroundImageName,
+                            textColor: textColor
+                        )
+                        .padding(.top, 24)
+                        
+                        WeatherDetailGridView(
+                            weatherData: weatherData,
+                            textColor: textColor
+                        )
+                        .padding(.top, 16)
+                        .padding(.bottom, 40)
                     }
-                    .padding(.top, 10)
-                    
-                    WeatherHeaderView(
-                        weatherData: weatherData,
-                        textColor: textColor
-                    )
-                    .padding(.top, 20)
-                    
-                    ThreeDayForecastView(
-                        forecastDays: weatherData.forecast,
-                        backgroundImageName: backgroundImageName,
-                        textColor: textColor
-                    )
-                    .padding(.top, 24)
-                    
-                    WeatherDetailGridView(
-                        weatherData: weatherData,
-                        textColor: textColor
-                    )
-                    .padding(.top, 16)
-                    .padding(.bottom, 40)
                 }
             }
         }
